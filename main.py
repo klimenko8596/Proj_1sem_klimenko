aded4989f70cd8715b44b32a3e287cba996f5776
klimenko8596 <94870321+klimenko8596@users.noreply.github.com>
2022-03-30 12:17:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# import module
+import figures
+# module.get_set()
+#
+# module.get_txt()
+#
+# print(module.list_doc.doc)
+#
+# print(module.file_doc.doc)
+#
+# print(dir(module))
+
+print(figures.circle_area())
+print(figures.circle_area(6))
+
+print(figures.triangle_area())
+print(figures.triangle_perimetr())
+
+print(figures.square_area())
+print(figures.square_area(7))
